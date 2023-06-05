cb2db9de222ade95b73a6adb0b41b4d3d9cf438e
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-06-05 08:35:07 -0400

    Update dlFromGD.sh

--- scripts/dlFromGD.sh
+++ scripts/dlFromGD.sh
@@ -5,10 +5,9 @@ fileid=`echo $link | awk -F '/' '{ print $6 }'`
 #fileid="118ul9uLt-jebHAOMQNTogfbXsjJpXzTf"  #eg, 1AMQ1OndXcTxXwklOGJvK-A5uCGvHR4I7
 filename=$2
 #filename="zwx_debra.ckpt"
-cd /workspace/runpod_ai/stable-diffusion-webui/models/Stable-diffusion/  #if downloading a checkpoint
-#cd /workspace/runpod_ai/stable-diffusion-webui/models/Lora  #if downloading a Lora
+destDir=$3
+#eg: /workspace/runpod_ai/stable-diffusion-webui/models/Stable-diffusion if a checkpoint
+#    /workspace/runpod_ai/stable-diffusion-webui/models/Lora if a Lora
+cd $destDir
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-#echo $html
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
-
-

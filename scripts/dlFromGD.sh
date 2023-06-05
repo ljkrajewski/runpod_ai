78ba389b2858afae064dce8ce21bd44dc9902c6c
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-06-05 07:14:59 -0400

    Update and rename dlFromGD.sh to scripts/dlFromGD.sh

--- scripts/dlFromGD.sh
+++ scripts/dlFromGD.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 fileid=$1
+# Example Google Drive link: https://drive.google.com/file/d/118ul9uLt-jebHAOMQNTogfbXsjJpXzTf/view?usp=sharing
 #fileid="118ul9uLt-jebHAOMQNTogfbXsjJpXzTf"  #eg, 1AMQ1OndXcTxXwklOGJvK-A5uCGvHR4I7
 filename=$2
 #filename="zwx_debra.ckpt"
 cd /workspace/runpod_ai/stable-diffusion-webui/models/Stable-diffusion/  #if downloading a checkpoint
-#cd /workspace/stable-diffusion-webui/models/Lora  #if downloading a Lora
+#cd /workspace/runpod_ai/stable-diffusion-webui/models/Lora  #if downloading a Lora
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 #echo $html
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}

7b2baef228c94e1b22bf151f332c492c642e0cde
Leo Krajewski <leo.krajewski@aol.com>
2023-07-11 06:51:40 +0000

    added models/extensions to a1111

--- auto1111.sh
+++ auto1111.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 cd /workspace
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
-cd stable-diffusion-webui/
+
+cd /workspace/stable-diffusion-webui/models/Stable-diffusion
+wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
+wget https://civitai.com/api/download/models/51913 && mv 51913 edgeOfRealism.safetensors
+
+cd /workspace/stable-diffusion-webui/extensions
+# Kohya-ss/Additional Networks models (https://github.com/kohya-ss/sd-webui-additional-networks.git)
+git clone https://github.com/kohya-ss/sd-webui-additional-networks.git
+# ControlNet for Stable Diffusion WebUI (https://github.com/Mikubill/sd-webui-controlnet)
+git clone https://github.com/Mikubill/sd-webui-controlnet.git
+cd sd-webui-controlnet/models
+git lfs install
+git clone https://huggingface.co/lllyasviel/ControlNet-v1-1
+
+cd /workspace/stable-diffusion-webui/
 cmdlineArgs="--xformers --enable-insecure-extension-access"
 sed -i 's/#export COMMANDLINE_ARGS=""/export COMMANDLINE_ARGS="--share $cmdlineArgs"/' webui-user.sh
 ./webui.sh -f

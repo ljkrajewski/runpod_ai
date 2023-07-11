372a9a451fb0d574d94fa60740ca7cd5d1cb2d59
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-07-11 08:45:05 -0400

    Update auto1111.sh

--- auto1111.sh
+++ auto1111.sh
@@ -4,11 +4,9 @@ function dlFromGoogle {
 #because Google is a pain in the @$$.
 #Downloads a file from Google drive to the current directory.
 #Usage: dlFromGoogle 'http://drive.google.com/link/to/file' 'filename.ext'
-  link=$1
-  filename=$2
-  fileid=`echo $link | awk -F '/' '{ print $6 }'`
+  fileid=`echo $1 | awk -F '/' '{ print $6 }'`
   html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=$fileid"`
-  curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo $html|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=$fileid" -o $filename
+  curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo $html|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=$fileid" -o $2
 }
 
 cd /workspace

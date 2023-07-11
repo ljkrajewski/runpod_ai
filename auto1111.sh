c2a6a74d21fe2f5269e4bf27c518b47374c76de2
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-07-11 08:32:04 -0400

    Update auto1111.sh

--- auto1111.sh
+++ auto1111.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
+
+function dlFromGoogle {
+#because Google is a pain in the @$$.
+#Usage: dlFromGoogle 'http://drive.google.com/link/to/file' 'filename.ext' '/dest/ina/tion/directory'
+  link=$1
+  filename=$2
+  destDir=$3
+  #echo Downloading from Google Drive
+  fileid=`echo $link | awk -F '/' '{ print $6 }'`
+  #echo File ID = $fileid
+  html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+  #echo html = $html
+  #echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'
+  curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+}
+
 cd /workspace
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
+## Models ##
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
 wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
 wget https://civitai.com/api/download/models/51913 && mv 51913 edgeOfRealism.safetensors
 
+## Extentions ##
 cd /workspace/stable-diffusion-webui/extensions
 # Kohya-ss/Additional Networks models (https://github.com/kohya-ss/sd-webui-additional-networks.git)
 git clone https://github.com/kohya-ss/sd-webui-additional-networks.git
@@ -15,6 +33,7 @@ cd sd-webui-controlnet/models
 git lfs install
 git clone https://huggingface.co/lllyasviel/ControlNet-v1-1
 
+## Start the GUI ##
 cd /workspace/stable-diffusion-webui/
 cmdlineArgs="--xformers --enable-insecure-extension-access"
 sed -i 's/#export COMMANDLINE_ARGS=""/export COMMANDLINE_ARGS="--share $cmdlineArgs"/' webui-user.sh

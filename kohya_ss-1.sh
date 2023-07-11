57613cb4aff9025ab3422b52e2b33fd9bb4ffd21
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-07-11 10:44:06 -0400

    Update kohya_ss-1.sh

--- kohya_ss-1.sh
+++ kohya_ss-1.sh
@@ -8,10 +8,11 @@ mkdir -p /workspace/training/img/tmp
 mkdir -p /workspace/training/model
 mkdir -p /workspace/training/reg/tmp
 echo "Upload your training images to /workspace/training/img/tmp now."
+echo "You can proceed when all training images are uploaded."
 pause
 
 numTrainingFiles=$((`ls -l /workspace/training/img/tmp | wc -l` - 1))
-#for LoRA training, btw 1500-6000 steps total
+#for LoRA training, btw 1500-6000 steps per epoch
 minRecRepeats=1500/$numTrainingFiles+1
 if [[ $minRecRepeats -lt 10 ]]; then
   minRecRepeats=10
@@ -20,22 +21,35 @@ maxRecRepeats=6000/$numTrainingFiles+1
 if [[ $maxRecRepeats -lt 10 ]]; then
   maxRecRepeats=10
 fi
+echo
 echo "Recommended number of repeats is between $minRecRepeats and $maxRecRepeats per image."
 echo "Note: Easy subjects like faces can use fewer repeats."
-read -p "Number of repeats per image:  " numRepeats
+read -p "Enter number of repeats per image:  " numRepeats
 cd mkdir -p /workspace/training/img
 mv tmp "$numRepeats_$tokenName $className"
 
-echo "Will need at least $($numRepeats * $numTrainingFiles) regularization images of class $className."
+echo
+echo "You will need at least $($numRepeats * $numTrainingFiles) regularization images of class $className."
 echo "Run /workspace/runpod_ai/auto1111.sh in a new terminal to install and run AUTOMATIC1111/Stable Diffusion to create your regularization images."
 echo "In another terminal, run /workspace/runpod_ai/kohya_ss-2.sh to install and run kohya_ss.
-echo "When your regularization images are complete, move them to /workspace/training/reg/tmp now."
+echo "When your regularization images are complete, move them to /workspace/training/reg/tmp."
+echo "You can proceed when the regularization images are moved."
 pause
 
 cd mkdir -p /workspace/training/reg
 mv tmp "1_$className"
 
+echo
+echo "Wait here until you are able to enter the koyha_ss UI."
+pause
+
+echo
+echo "Last few steps..."
+echo "1) Caption your training files."
+echo "Captioning directory: ""/workspace/training/img/$numRepeats_$tokenName $className"""
+echo "Remember to add ""$tokenName"" to the captioning prefix along with any other words you wish to train on."
+echo
 sed 's/TOKEN-NAME/$tokenName/g' /workspace/runpod_ai/kohya-ss.conf.orig > /workspace/kohya-ss.conf
-echo "Load configuration file /workspace/kohya-ss.conf into hohya-ss."
+echo "2) Load the configuration file at /workspace/kohya-ss.conf into hohya-ss."
 echo "Confirm your sample prompt, make any other parameter changes you wish to, then begin training."
 

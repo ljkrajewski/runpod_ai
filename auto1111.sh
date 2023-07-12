53bbe467c03d35e1380e1471a030531a11a4ab5c
Leo Krajewski <leo.krajewski@aol.com>
2023-07-12 23:26:52 +0000

    Testing...

--- auto1111.sh
+++ auto1111.sh
@@ -10,7 +10,7 @@ function dlFromGoogle {
 }
 
 cd /workspace
-git lfs install
+#git lfs install
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
 ## Models ##
@@ -23,21 +23,21 @@ cd /workspace/stable-diffusion-webui/models/VAE
 wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
 
 ## Embeddings ##
-cd /workspace/stable-diffusion-webui/embeddings
-git clone https://huggingface.co/nolanaatama/embeddings
+#cd /workspace/stable-diffusion-webui/embeddings
+#git clone https://huggingface.co/nolanaatama/embeddings
 
 ## Extentions ##
 cd /workspace/stable-diffusion-webui/extensions
 # Kohya-ss/Additional Networks models (https://github.com/kohya-ss/sd-webui-additional-networks.git)
 git clone https://github.com/kohya-ss/sd-webui-additional-networks.git
 # Booru Tag Autocomplete
-# -- get URL from SD extentions
+git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git
 # Image browser
-# -- get URL from SD extentions
+git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git
 # Aspect Ratio Helper
-# -- get URL from SD extentions
+git clone https://github.com/thomasasfk/sd-webui-aspect-ratio-helper.git
 # System Info
-# -- get URL from SD extentions
+git clone https://github.com/vladmandic/sd-extension-system-info.git
 # ControlNet for Stable Diffusion WebUI (https://github.com/Mikubill/sd-webui-controlnet)
 git clone https://github.com/Mikubill/sd-webui-controlnet.git
 cd sd-webui-controlnet/models
@@ -45,6 +45,8 @@ git clone https://huggingface.co/lllyasviel/ControlNet-v1-1
 
 ## Start the GUI ##
 cd /workspace/stable-diffusion-webui/
-cmdlineArgs="--xformers --enable-insecure-extension-access"
+cp /workspace/runpod_ai/settings/config.json .
+cp /workspace/runpod_ai/emb.txt extensions/a1111-sd-webui-tagcomplete/tags/temp
+cmdlineArgs="--xformers --enable-insecure-extension-access --disable-safe-unpickle"
 sed -i 's/#export COMMANDLINE_ARGS=""/export COMMANDLINE_ARGS="--share $cmdlineArgs"/' webui-user.sh
 ./webui.sh -f
--- kohya_ss-1.sh
+++ kohya_ss-1.sh
@@ -9,15 +9,15 @@ mkdir -p /workspace/training/model
 mkdir -p /workspace/training/reg/tmp
 echo "Upload your training images to /workspace/training/img/tmp now."
 echo "You can proceed when all training images are uploaded."
-pause
+read -p "Press <enter> to continue" tmpPause
 
 numTrainingFiles=$((`ls -l /workspace/training/img/tmp | wc -l` - 1))
 #for LoRA training, btw 1500-6000 steps per epoch
-minRecRepeats=1500/$numTrainingFiles+1
+minRecRepeats=$(((1500/$numTrainingFiles)+1))
 if [[ "$minRecRepeats" -lt "10" ]]; then
   minRecRepeats=10
 fi
-maxRecRepeats=6000/$numTrainingFiles+1
+maxRecRepeats=$(((6000/$numTrainingFiles)+1))
 if [[ "$maxRecRepeats" -lt "10" ]]; then
   maxRecRepeats=10
 fi
@@ -26,32 +26,34 @@ echo "Recommended number of repeats is between $minRecRepeats and $maxRecRepeats
 echo "Note: Easy subjects like faces can use fewer repeats."
 read -p "Enter number of repeats per image:  " numRepeats
 cd /workspace/training/img
-mv tmp "$numRepeats_$tokenName $className"
+imgDirName=$numRepeats"_$tokenName $className"
+mv tmp "$imgDirName"
 
+numRegFiles=$(($numRepeats * $numTrainingFiles))
 echo
-echo "You will need at least $($numRepeats * $numTrainingFiles) regularization images of class $className."
+echo "You will need at least $numRegFiles regularization images of class $className."
 echo "Run /workspace/runpod_ai/auto1111.sh in a new terminal to install and run AUTOMATIC1111/Stable Diffusion to create your regularization images."
-echo "In another terminal, run /workspace/runpod_ai/kohya_ss-2.sh to install and run kohya_ss.
+echo "In another terminal, run /workspace/runpod_ai/kohya_ss-2.sh to install and run kohya_ss."
 echo "When your regularization images are complete, move them to /workspace/training/reg/tmp."
 echo "You can proceed when the regularization images are moved."
-pause
+read -p "Press <enter> to continue" tmpPause
 
 cd /workspace/training/reg
 mv tmp "1_$className"
 
 echo
 echo "Wait here until you are able to enter the koyha_ss UI."
-pause
+read -p "Press <enter> to continue" tmpPause
 
 echo
 echo "Last few steps..."
-echo "1) Caption your training files."
-echo "Captioning directory: ""/workspace/training/img/$numRepeats_$tokenName $className"""
+echo "1\) Caption your training files."
+echo "Captioning directory: /workspace/training/img/$imgDirName"
 echo "BLIP is recommended for phrotorealistic models, WD14 for anime/drawings."
-echo "Remember to add ""$tokenName"" to the captioning prefix along with any other words you wish to train on."
+echo "Remember to add '$tokenName' to the captioning prefix along with any other words you wish to train on."
 echo
-sed 's/TOKENNAME/$tokenName/g' /workspace/runpod_ai/kohya-ss.conf.orig > /workspace/kohya-ss.conf
-sed -i 's/CLASSNAME/$className/g' /workspace/kohya-ss.conf
-echo "2) Load the configuration file at /workspace/kohya-ss.conf into hohya-ss."
+sed "s/TOKENNAME/$tokenName/g" /workspace/runpod_ai/settings/kohya-ss.conf.orig > /workspace/kohya-ss.conf
+sed -i "s/CLASSNAME/$className/g" /workspace/kohya-ss.conf
+echo "2\) Load the configuration file at /workspace/kohya-ss.conf into hohya-ss."
 echo "Confirm your sample prompt, make any other parameter changes you wish to, then begin training."
 

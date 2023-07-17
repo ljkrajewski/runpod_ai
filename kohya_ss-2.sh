bfe98cbd2bdb3c1dcb20e41415389356bc56bf00
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-07-17 14:56:27 -0400

    Update kohya_ss-2.sh

--- kohya_ss-2.sh
+++ kohya_ss-2.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd /workspace
 git clone https://github.com/bmaltais/kohya_ss.git
-wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
+#wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
+wget https://civitai.com/api/download/models/90072 && mv 90072 Photon.safetensors
 
 cd kohya_ss/
 ./setup-runpod.sh

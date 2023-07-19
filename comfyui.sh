503fff75bfa18c796a80e6b3d36c7370a834b7a8
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-07-19 14:43:19 -0400

    Update comfyui.sh

--- comfyui.sh
+++ comfyui.sh
@@ -9,6 +9,14 @@ fuser -k 3000/tcp
 # Manual install - https://github.com/FurkanGozukara/Stable-Diffusion/blob/main/Tutorials/How-To-Use-ComfyUI-On-Your-PC-On-RunPod-On-Colab-With-SDXL.md
 cd /workspace
 git clone https://github.com/comfyanonymous/ComfyUI.git
+
+cd /workspace/ComfyUI/models/checkpoints
+wget https://$USERNAME:$TOKEN@huggingface.co/stabilityai/stable-diffusion-xl-base-0.9/resolve/main/sd_xl_base_0.9.safetensors
+wget https://$USERNAME:$TOKEN@huggingface.co/stabilityai/stable-diffusion-xl-refiner-0.9/resolve/main/sd_xl_refiner_0.9.safetensors
+wget https://huggingface.co/SG161222/Realistic_Vision_V4.0/resolve/main/Realistic_Vision_V4.0.safetensors
+cd /workspace/ComfyUI/models/vae
+wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
+
 cd /workspace/ComfyUI
 python -m venv venv
 cd venv
@@ -20,13 +28,6 @@ cd ..
 # If you get the "Torch not compiled with CUDA enabled" error, uninstall torch with:
 #pip uninstall torch
 pip install -r requirements.txt
-
-cd /workspace/ComfyUI/models/checkpoints
-wget https://$USERNAME:$TOKEN@huggingface.co/stabilityai/stable-diffusion-xl-base-0.9/resolve/main/sd_xl_base_0.9.safetensors
-wget https://$USERNAME:$TOKEN@huggingface.co/stabilityai/stable-diffusion-xl-refiner-0.9/resolve/main/sd_xl_refiner_0.9.safetensors
-wget https://huggingface.co/SG161222/Realistic_Vision_V4.0/resolve/main/Realistic_Vision_V4.0.safetensors
-cd /workspace/ComfyUI/models/vae
-wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
 
 cd /workspace/ComfyUI
 python main.py --listen 0.0.0.0 --port 3000

ce70521c2ed51d27b9bf91935ba806c230ae6044
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-07-19 07:46:34 -0400

    Update comfyui.sh

--- comfyui.sh
+++ comfyui.sh
@@ -4,6 +4,6 @@
 cd /workspace
 git clone https://github.com/comfyanonymous/ComfyUI.git
 cd /workspace/ComfyUI/models/checkpoints
-# Download checkpoints
+wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-0.9/resolve/main/sd_xl_base_0.9.safetensors
 cd /workspace/ComfyUI/models/vae
 wget https://huggingface.co/YANGYINGDUO/vae-ft-mse-840000-ema-pruned.ckpt/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt

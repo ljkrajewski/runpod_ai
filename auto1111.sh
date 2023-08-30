e913c812e457ce0cf18c7dfe79901280c3bdb7ff
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-08-30 06:28:12 -0400

    Update auto1111.sh

--- auto1111.sh
+++ auto1111.sh
@@ -35,6 +35,8 @@ curl https://civitai.com/api/download/models/146074 -o EpicPhotoGasm-inpanting.s
 ## VAE ##
 cd $SDBASE/models/VAE
 wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
+wget https://huggingface.co/LarryAIDraw/anything-v4.0.vae/resolve/main/anything-v4.0.vae.pt
+wget https://huggingface.co/iZELX1/Grapefruit/raw/main/Grapefruit.vae.pt
 
 ## LoRAs ##
 mkdir -p $SDBASE/models/Lora
@@ -50,6 +52,12 @@ curl https://civitai.com/api/download/models/77169 -o BadDream.pt -L
 curl https://civitai.com/api/download/models/77173 -o UnrealisticDream.pt -L
 curl https://civitai.com/api/download/models/94057 -o FastNegativeV2.pt -L
 curl https://civitai.com/api/download/models/125849 -o Bad-Hands-5.pt -L
+
+## Upscalers ##
+mkdir -p $SDBASE/models/ESRGAN
+cd $SDBASE/models/ESRGAN
+wget https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/8x_NMKD-Superscale_150000_G.pth
+wget https://nmkd.de/shared/ESRGAN/Models/Faces/8x_NMKD-Faces_160000_G.pth
 
 ## Extentions ##
 cd $SDBASE/extensions

8dda2cb529e4806128dac17dd58886b0e382718d
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-08-31 07:54:56 -0400

    Update comfyui.sh

--- comfyui.sh
+++ comfyui.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 # ComfyUI for SD
-#apt install psmisc -y
-#fuser -k 3000/tcp
 
 BASE='/workspace'
 CUIBASE="$BASE/ComfyUI"
 RPBASE="$BASE/runpod_ai"
-#source $RPBASE/initial_setup.sh
 
 # Manual install - https://github.com/FurkanGozukara/Stable-Diffusion/blob/main/Tutorials/How-To-Use-ComfyUI-On-Your-PC-On-RunPod-On-Colab-With-SDXL.md
 cd $BASE
 git clone https://github.com/comfyanonymous/ComfyUI.git
 # Change default workflow
 mv $CUIBASE/web/scripts/defaultGraph.js{,.bak}
-#sed 's/^{$/export const defaultGraph = {/' $BASE/runpod_ai/settings/ComfyUI_Workflow_SDXL.json > $CUIBASE/web/scripts/defaultGraph.js
 sed 's/^{$/export const defaultGraph = {/' $BASE/runpod_ai/settings/ComfyUI_default.json > $CUIBASE/web/scripts/defaultGraph.js
 cd $CUIBASE
 python -m venv venv
@@ -31,50 +27,6 @@ EMBEDDING_DIR="$CUIBASE/models/embeddings"
 UPSCALERS_DIR="$CUIBASE/models/upscale_models"
 source $RPBASE/sd-config-models.sh
 
-:<<COMMENT
-cd $CUIBASE/models/checkpoints
-#wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-#wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
-wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
-#wget https://huggingface.co/SG161222/Realistic_Vision_V4.0/resolve/main/Realistic_Vision_V4.0.safetensors
-#curl https://civitai.com/api/download/models/90072 -o Photon.safetensors -L
-#curl https://civitai.com/models/25694 -o EpicRealism.safetensors -L
-# curl https://civitai.com/models/46422?modelVersionId=117037 -o Juggernaut.safetensors -L
-# curl https://civitai.com/models/10540 -o Verisimilitude.safetensors -L
-#curl https://civitai.com/models/81458/absolutereality -o AbsoluteReality.safetensors -L
-# curl https://civitai.com/models/22922?modelVersionId=72396 -o Lyriel.safetensors -L
-#curl https://civitai.com/models/21813?modelVersionId=51913 -o EdgeOfRealism.safetensors -L
-# curl https://civitai.com/models/4823?modelVersionId=15236 -o Deliberate.safetensors -L
-# curl https://civitai.com/models/4384?modelVersionId=94081 -o DreamShaper.safetensors -L
-# curl https://civitai.com/models/36520/ghostmix -o GhostMix.safetensors -L
-# curl https://civitai.com/models/57319?modelVersionId=99805 -o A-ZovyaPhotoreal.safetensors -L
-# curl https://civitai.com/models/15022?modelVersionId=53814 -o 526Mix.safetensors -L
-curl https://civitai.com/api/download/models/130072 -o RealisticVisionV5-1.safetensors -L
-curl https://civitai.com/api/download/models/130090 -o RealisticVisionV5-1-inpainting.safetensors -L
-#curl https://civitai.com/api/download/models/29682 -o LifeLikeDiffusion.safetensors -L
-curl https://civitai.com/api/download/models/145885 -o EpicPhotoGasm.safetensors -L
-curl https://civitai.com/api/download/models/146074 -o EpicPhotoGasm-inpanting.safetensors -L
-
-cd $CUIBASE/models/embeddings
-#git clone https://huggingface.co/nolanaatama/embeddings
-curl https://civitai.com/api/download/models/77169 -o BadDream.pt -L
-curl https://civitai.com/api/download/models/77173 -o UnrealisticDream.pt -L
-curl https://civitai.com/api/download/models/94057 -o FastNegativeV2.pt -L
-curl https://civitai.com/api/download/models/125849 -o Bad-Hands-5.pt -L
-
-cd $CUIBASE/models/vae
-wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
-
-cd $CUIBASE/models/loras
-curl https://civitai.com/api/download/models/63006 -o LowRA.safetensors -L
-curl https://civitai.com/api/download/models/17988 -o 20D.safetensors -L
-curl https://civitai.com/api/download/models/62833 -o DetailTweaker.safetensors -L
-
-cd $CUIBASE/models/upscale_models
-curl https://civitai.com/api/download/models/70526 -o VariousUpscalers.zip -L
-unzip VariousUpscalers.zip
-COMMENT
-
 cd $CUIBASE/custom_nodes
 # -- ComfyUI Manager
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
@@ -85,21 +37,14 @@ git submodule update --init --recursive
 python install.py
 cp impact-pack.ini{,.bu}
 sed -i 's/mmdet_skip = True/mmdet_skip = False/' impact-pack.ini
-#mkdir -p $CUIBASE/models/mmdets/bbox
-#cd $CUIBASE/models/mmdets/bbox
-#wget https://huggingface.co/dustysys/ddetailer/resolve/main/mmdet/bbox/mmdet_anime-face_yolov3.pth
-#wget https://raw.githubusercontent.com/Bing-su/dddetailer/master/config/mmdet_anime-face_yolov3.py
-#mkdir -p $CUIBASE/models/sams
-#cd $CUIBASE/models/sams
-#wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
 cd $CUIBASE/custom_nodes
 # -- ComfyUI FaceRestore Node - https://civitai.com/models/24690/comfyui-facerestore-node
 curl https://civitai.com/api/download/models/122586  -o FaceRestore.zip -L
 unzip FaceRestore.zip
-[ -d "$CUIBASE/models/facerestore_models/" ] || mkdir "$CUIBASE/models/facerestore_models/" && cd "$CUIBASE/models/facerestore_models/"
+[ ! -d "$CUIBASE/models/facerestore_models/" ] && mkdir "$CUIBASE/models/facerestore_models/" && cd "$CUIBASE/models/facerestore_models/"
 wget https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
 wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
-[ -d "$CUIBASE/models/facedetection/" ] || mkdir "$CUIBASE/models/facedetection/" && cd "$CUIBASE/models/facedetection/"
+[ ! -d "$CUIBASE/models/facedetection/" ] && mkdir "$CUIBASE/models/facedetection/" && cd "$CUIBASE/models/facedetection/"
 wget https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth
 wget https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_mobilenet0.25_Final.pth
 wget https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/yolov5l-face.pth

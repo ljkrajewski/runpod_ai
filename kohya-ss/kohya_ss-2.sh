6542ffba9b31cd1d47a313c8ae92a86c2d4819ed
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-09-07 08:20:12 -0400

    Update and rename kohya_ss-2.sh to kohya-ss/kohya_ss-2.sh

--- kohya-ss/kohya_ss-2.sh
+++ kohya-ss/kohya_ss-2.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 BASE="/workspace"
+RPBASE="$BASE/runpod_ai/kohya_ss"
+KSSBASE="$BASE/kohya_ss"
+TRAININGDIR="$BASE/training"
 
 cd $BASE
 git clone https://github.com/bmaltais/kohya_ss.git
-cd kohya_ss
+cd $KSSBASE
 python -m venv venv
 cd venv
 source bin/activate
 
-cd $BASE
+cd $TRAININGDIR
 [ ! -f v1-5-pruned.safetensors ] && \
   wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors
 #curl https://civitai.com/api/download/models/90072 -o Photon.safetensors -L
 
-cd kohya_ss/
+cd $KSSBASE
 ./setup-runpod.sh
 ./gui.sh --share --headless

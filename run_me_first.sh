60e0067c1fac4ba136cec18e4327a4e1a18a2a27
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-10-04 16:55:12 -0400

    Update run_me_first.sh

--- run_me_first.sh
+++ run_me_first.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-apt update && apt upgrade git -y && apt install tmux vim psmisc less unzip -y
+apt update && apt upgrade git -y && apt install tmux vim psmisc less unzip git-lfs -y
 python -m pip install --upgrade pip

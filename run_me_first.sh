9655a942bbf1d88ffcbaa5fe146592d5912a4c2e
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-10-05 13:35:04 -0400

    Update run_me_first.sh

--- run_me_first.sh
+++ run_me_first.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 apt update && apt upgrade git -y && apt install tmux vim psmisc less unzip git-lfs -y
 python -m pip install --upgrade pip
-pip install virtualenv
+python -m pip install virtualenv

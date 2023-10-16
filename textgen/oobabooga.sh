544f2c097cfdaf1d47aa96324658d311577e5571
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-10-16 15:29:12 -0400

    Update oobabooga.sh

--- textgen/oobabooga.sh
+++ textgen/oobabooga.sh
@@ -1,41 +1,57 @@
 #!/usr/bin/env bash
-fuser -k 7860/tcp
+
+#MODEL='TheBloke/WizardCoder-15B-1.0-GPTQ'
+#   https://huggingface.co/TheBloke/WizardCoder-15B-1.0-GPTQ#how-to-easily-download-and-use-this-model-in-text-generation-webui
+#MODEL='WizardLM/WizardCoder-Python-34B-V1.0'   #Original: WizardLM/WizardCoder-Python-34B-V1.0
+#MODEL='TheBloke/WizardCoder-Python-34B-V1.0-GPTQ'   #Original: WizardLM/WizardCoder-Python-34B-V1.0
+#   https://huggingface.co/TheBloke/WizardCoder-Python-34B-V1.0-GPTQ#how-to-easily-download-and-use-this-model-in-text-generation-webui
+#MODEL='TheBloke/Pygmalion-2-13B-GPTQ'
+#MODEL='TehVenom/Pygmalion-7b-4bit-GPTQ-Safetensors'
+MODEL='TheBloke/Pygmalion-2-13B-SuperCOT-weighed-GPTQ'
+#MODEL='TheBloke/guanaco-65B-GPTQ'   #requires 48GB VRAM
 
 BASE='/workspace'
-INSTALLBASE="$BASE/oobabooga_linux"
-TGWBASE="$BASE/oobabooga_linux/text-generation-webui"
-RPBASE="$BASE/runpod_ai/textget"
+TGWBASE="$BASE/text-generation-webui"
+MODEL_DIR=$(echo $MODEL | awk -F '/' '{ print $2 }')
 
+## Install Oobabooga & start virtual environment
 cd $BASE
-#git clone https://github.com/oobabooga/text-generation-webui.git
-wget https://github.com/oobabooga/text-generation-webui/releases/download/installers/oobabooga_linux.zip
-unzip oobabooga_linux.zip
+git clone https://github.com/oobabooga/text-generation-webui.git
+cd $TGWBASE
+python -m venv venv
+source venv/bin/activate
+pip install scipy
 
 # -- GPTQ-for-LLaMA
-cd $INSTALLBASE
+cd $TGWBASE
 pip install -r requirements.txt
 mkdir repositories
 cd repositories
 git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda
 cd GPTQ-for-LLaMa && python setup_cuda.py install
 
-# -- Install
-cp webui.py{,.bak}
-sed -i 's/gpuchoice = input("Input> ").lower()/gpuchoice = "a"/' webui.py
-sed -i 's/^        launch_webui()/#        launch_webui()/' webui.py
-bash start_linux.sh
-
-# -- Models
+## Download model from Hugging Face
 cd $TGWBASE/models
-#git clone --single-branch --branch gptq-4bit-32g-actorder_True https://huggingface.co/TheBloke/Pygmalion-2-13B-GPTQ
-#git clone --single-branch --branch main https://huggingface.co/TehVenom/Pygmalion-7b-4bit-GPTQ-Safetensors
-git clone --single-branch --branch main https://huggingface.co/TheBloke/guanaco-65B-GPTQ   #requires 48GB VRAM
+git lfs install
+git clone --single-branch --branch main https://huggingface.co/$MODEL
+##  If git chokes on the large files...
+#export GIT_LFS_SKIP_SMUDGE=1; git clone --single-branch --branch main https://huggingface.co/$MODEL
+#cd $MODEL_DIR
+#wget https://huggingface.co/$MODEL/resolve/main/model.safetensors
 
+## Command-line flag options:  https://github.com/oobabooga/text-generation-webui#starting-the-web-ui
 cd $TGWBASE
-pip install scipy
+#echo "--listen --chat --model llama" >> CMD_FLAGS.txt
+#echo "--listen --chat --auto-devices --cpu --model $MODEL_DIR" >> CMD_FLAGS.txt
+echo "--listen --chat --cpu --model $MODEL_DIR" >> CMD_FLAGS.txt
+#echo "--listen --listen-port 7860 --chat --cpu --model $MODEL_DIR" >> CMD_FLAGS.txt
 
-# -- Start UI
-cd $INSTALLBASE
-cp webui.py{.bak,}
-export OOBABOOGA_FLAGS="--listen -chat --auto-devices --model llama"
+## GPU_CHOICE options
+#    A) NVIDIA
+#    B) AMD (Linux/MacOS only. Requires ROCm SDK 5.6 on Linux)
+#    C) Apple M Series
+#    D) Intel Arc (IPEX)
+#    N) None (I want to run models in CPU mode)
+export GPU_CHOICE='A'
+
 bash start_linux.sh

d6e3de962f1844036d70a5622f05282dbf98acb6
ljkrajewski <50240322+ljkrajewski@users.noreply.github.com>
2023-10-17 14:04:57 -0400

    Update oobabooga.sh

--- textgen/oobabooga.sh
+++ textgen/oobabooga.sh
@@ -22,13 +22,13 @@ python -m venv venv
 source venv/bin/activate
 pip install scipy
 
-# -- GPTQ-for-LLaMA
-cd $TGWBASE
-pip install -r requirements.txt
-mkdir repositories
-cd repositories
-git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda
-cd GPTQ-for-LLaMa && python setup_cuda.py install
+## -- GPTQ-for-LLaMA
+#cd $TGWBASE
+#pip install -r requirements.txt
+#mkdir repositories
+#cd repositories
+#git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda
+#cd GPTQ-for-LLaMa && python setup_cuda.py install
 
 ## Download model from Hugging Face
 cd $TGWBASE/models
